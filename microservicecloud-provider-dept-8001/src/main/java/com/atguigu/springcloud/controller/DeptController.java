370fa89df1ea3d989d578180a9388fef5f63e908
akqjxx <liuer2595919@163.com>
2019-06-11 22:46:48 +0800

    dsfds

---
+++ microservicecloud-provider-dept-8001/src/main/java/com/atguigu/springcloud/controller/DeptController.java
@@ -0,0 +1,34 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.entities.Dept;
+import com.atguigu.springcloud.service.DeptService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ *
+ */
+@RestController
+public class DeptController {
+    @Autowired
+    private DeptService service;
+
+    @RequestMapping(value = "/dept/add", method = RequestMethod.POST)
+    public boolean add(@RequestBody Dept dept) {
+        return service.add(dept);
+    }
+
+    @RequestMapping(value = "/dept/get/{id}", method = RequestMethod.GET)
+    public Dept get(@PathVariable("id") Long id) {
+        return service.get(id);
+    }
+
+    @RequestMapping(value = "/dept/list", method = RequestMethod.GET)
+    public List<Dept> list() {
+        return service.list();
+    }
+
+
+}
