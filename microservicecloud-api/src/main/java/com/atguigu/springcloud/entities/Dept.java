af5f3dded5ffae00999f6d485c53f34358fc15d9
akqjxx <liuer2595919@163.com>
2019-06-11 18:32:04 +0800

    下班提交

--- microservicecloud-api/src/main/java/com/atguigu/springcloud/entities/Dept.java
+++ microservicecloud-api/src/main/java/com/atguigu/springcloud/entities/Dept.java
@@ -17,6 +17,7 @@ public class Dept implements Serializable// entity --orm--- db_table
 	private Long 	deptno; // 主键
 	private String 	dname; // 部门名称
 	private String 	db_source;// 来自那个数据库，因为微服务架构可以一个服务对应一个数据库，同一个信息被存储到不同数据库
+	private Integer id;
 	public Dept(String dname)
 	{
 		super();
@@ -27,6 +28,14 @@ public class Dept implements Serializable// entity --orm--- db_table
 }
 
 /**
+ *
+ *
+ *
+ *
+ * http://www.liaoxuefeng.com/wiki/0013739516305929606dd18361248578c67b8067c8c017b000/00137396287703354d8c6c01c904c7d9ff056ae23da865a000
+ *
+ * 　　http://blog.csdn.net/zongzhankui/article/details/46341967
+ *
  9.提交修改文件到GitHub
 
  　　新增文件（红色），右键-->Git-->add，将新增的文件加入本地仓库，此时文件变绿色
