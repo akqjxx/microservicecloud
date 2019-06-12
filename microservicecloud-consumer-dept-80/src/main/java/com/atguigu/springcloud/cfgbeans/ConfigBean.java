b6f0fa25ed3975b4442de679e85c4396f186c66b
akqjxx <liuer2595919@163.com>
2019-06-12 15:53:43 +0800

    中午提交提交

--- microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/cfgbeans/ConfigBean.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/cfgbeans/ConfigBean.java
@@ -4,10 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import static com.atguigu.springcloud.cfgbeans.InterfaceTest.DEFAULT_PREFIX;
+
 @Configuration
 public class ConfigBean {
     @Bean
     public RestTemplate getRestTemplate() {
+        String str = "t"+DEFAULT_PREFIX;
         return new RestTemplate();
     }
 }
---
+++ microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/cfgbeans/InterfaceTest.java
@@ -0,0 +1,6 @@
+package com.atguigu.springcloud.cfgbeans;
+
+public interface InterfaceTest {
+
+    String DEFAULT_PREFIX = "/eureka";
+}
